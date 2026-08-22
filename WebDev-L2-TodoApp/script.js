5f5468f5ba2de9a9ed384f178a66fdf799716f68
riya <griya3478@gmail.com>
2026-08-22 22:36:24 +0530

    feat: implement the edit features and delete features and completed task

--- WebDev-L2-TodoApp/script.js
+++ WebDev-L2-TodoApp/script.js
@@ -22,6 +22,17 @@ submitBtn.addEventListener("click", () => {
 //fucntions
 // add task
 
+function formatDateTime(date) {
+  return new Date(date).toLocaleString("en-IN", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
+
 function addTask() {
   let data = input.value.trim();
   if (data === "") {
@@ -34,10 +45,11 @@ function addTask() {
     text: data,
     status: "pending",
     createdAt: new Date(),
+    editedAt: null,
   };
-  console.log(taskObject);
+  // console.log(taskObject);
   tasks.push(taskObject);
-  console.log(tasks);
+  // console.log(tasks);
   saveTasks();
   renderTasks();
   updateCounts();
@@ -52,7 +64,17 @@ function saveTasks() {
 //createTaskCard
 function createTaskCard(task) {
   let actions = "";
+  let editedInfo = "";
 
+  const formattedCreatedDate = formatDateTime(task.createdAt);
+
+  if (task.editedAt) {
+    editedInfo = `
+    <p class="task-time">
+      Edited: ${formatDateTime(task.editedAt)}
+    </p>
+  `;
+  }
   if (task.status === "pending") {
     actions = `
       <button class="complete-btn" data-action="complete">
@@ -82,11 +104,14 @@ function createTaskCard(task) {
   const html = `
     <article class="task-item" data-task-id="${task.id}">
       <div class="task-content">
-      <input class="edit-input" value="Learn JavaScript">
+        <h3 class="task-title">${task.text}</h3>
+
         <p class="task-time">
-          ${task.status === "pending" ? "Added" : "Completed"}:
-          ${task.createdAt}
+          Added: ${formattedCreatedDate}
         </p>
+
+        ${editedInfo}
+
       </div>
 
       <div class="task-actions">
@@ -156,7 +181,7 @@ function updateCounts() {
 //parent listener
 pendingList.addEventListener("click", (event) => {
   let clickElement = event.target;
-  console.log(clickElement);
+  // console.log(clickElement);
   if (clickElement.dataset.action === "complete") {
     let article = event.target.closest(".task-item");
     let id = Number(article.dataset.taskId);
@@ -168,7 +193,33 @@ pendingList.addEventListener("click", (event) => {
     let titleElement = article.querySelector(".task-title");
     let text = titleElement.textContent;
     console.log(text);
-    editTask(id, text);
+    const editHtml = editCard(id, text);
+    article.insertAdjacentHTML("beforebegin", editHtml);
+    const editInput = article.parentElement.querySelector(".edit-input");
+    editInput.focus();
+    editInput.setSelectionRange(editInput.value.length, editInput.value.length);
+    article.remove();
+  }
+  if (clickElement.dataset.action === "save-edit") {
+    const editArticle = event.target.closest(".edit-task-card");
+    const id = Number(editArticle.dataset.taskId);
+    const inputElement = editArticle.querySelector(".edit-input");
+    const newText = inputElement.value;
+    editTask(id, newText);
+  }
+  if (clickElement.dataset.action === "cancel-edit") {
+    const editArticle = event.target.closest(".edit-task-card");
+
+    const id = Number(editArticle.dataset.taskId);
+
+    editArticle.remove();
+
+    renderTasks();
+  }
+  if (clickElement.dataset.action === "delete") {
+    let article = event.target.closest(".task-item");
+    let id = Number(article.dataset.taskId);
+    deleteTask(id);
   }
 });
 
@@ -180,8 +231,36 @@ completedList.addEventListener("click", (event) => {
     let id = Number(article.dataset.taskId);
     let titleElement = article.querySelector(".task-title");
     let text = titleElement.textContent;
-    console.log(text);
-    // editTask(id, text);
+    console.log(id, text);
+    const editHtml = editCard(id, text);
+    article.insertAdjacentHTML("beforebegin", editHtml);
+    const editInput = article.parentElement.querySelector(".edit-input");
+    editInput.focus();
+    editInput.setSelectionRange(editInput.value.length, editInput.value.length);
+    article.remove();
+  }
+  if (clickElement.dataset.action === "save-edit") {
+    const editArticle = event.target.closest(".edit-task-card");
+    const inputElement = editArticle.querySelector(".edit-input");
+    const id = Number(editArticle.dataset.taskId);
+    const newText = inputElement.value;
+    console.log(newText);
+
+    editTask(id, newText);
+  }
+  if (clickElement.dataset.action === "cancel-edit") {
+    const editArticle = event.target.closest(".edit-task-card");
+
+    const id = Number(editArticle.dataset.taskId);
+
+    editArticle.remove();
+
+    renderTasks();
+  }
+  if (clickElement.dataset.action === "delete") {
+    let article = event.target.closest(".task-item");
+    let id = Number(article.dataset.taskId);
+    deleteTask(id);
   }
 });
 
@@ -197,8 +276,42 @@ function toggleTask(taskId) {
 }
 
 //editTask
+function editCard(id, text) {
+  let html = `
+  <article class="edit-task-card" data-task-id="${id}">
+  <div class="edit-content">
+    <input
+    type="text"
+      class="edit-input"
+      value="${text}"
+    />
+
+    <div class="edit-actions">
+      <button
+      type="button"
+        class="save-edit-btn"
+        data-action="save-edit"
+      >
+        Save
+      </button>
+      
+      <button
+        type="button"
+        class="cancel-edit-btn"
+        data-action="cancel-edit"
+        >
+        Cancel
+        </button>
+    </div>
+  </div>
+</article>
+`;
+  return html;
+}
+
 function editTask(taskId, newText) {
   newText = newText.trim();
+  console.log(newText);
   if (!newText) {
     return;
   }
@@ -206,9 +319,23 @@ function editTask(taskId, newText) {
   if (!task) {
     return;
   }
+  if (task.text === newText) {
+    return;
+  }
+
   task.text = newText;
+  task.editedAt = new Date();
   saveTasks();
   renderTasks();
 }
 
 //deleteTask
+function deleteTask(id) {
+  const remainingTask = tasks.filter((task) => task.id !== id);
+
+  tasks.length = 0;
+  tasks.push(...remainingTask);
+  saveTasks();
+  renderTasks();
+  updateCounts();
+}
